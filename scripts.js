c48bd2f01882567a3361131e0dfe53bc04162127
tomas0008 <133876500+tomas0008@users.noreply.github.com>
2024-12-04 13:47:52 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -89,3 +89,66 @@ function reset(elemId, multElemId, nextUpId, clickScoreId, upgradesId)
     elem(clickScoreId).innerHTML = 1;
     elem(upgradesId).innerHTML = 0;
 }
+
+function setCookie(cname, cvalue)
+{
+    document.cookie = cname + "=" + cvalue + ";";
+}
+
+function getCookie(cname)
+{
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++)
+    {
+        let c = ca[i];
+        while (c.charAt(0) == ' ')
+        {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0)
+        {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function save()
+{
+    setCookie("clickScore", clickScore);
+    setCookie("clicks", clicks);
+    setCookie("mult", mult);
+    setCookie("boost", boost);
+    setCookie("upgrades", upgrades);
+    setCookie("level", level);
+}
+
+function load()
+{
+    clickScore = getCookie("clickScore");
+    clickScore = clickScore == "" ? 1 : Number(clickScore);
+
+    clicks = getCookie("clicks");
+    clicks = clickScore == "" ? 0 : Number(clicks);
+
+    mult = getCookie("mult");
+    mult = mult == "" ? 1 : Number(mult);
+
+    boost = getCookie("boost");
+    boost = boost == "" ? 1 : Number(boost);
+
+    upgrades = getCookie("upgrades");
+    upgrades = upgrades == "" ? 0 : Number(upgrades);
+
+    level = getCookie("level");
+    level = level == "" ? 1 : Number(level);
+}
+
+function display(clickScoreId, multId, counterId, nextUpId, upId, boostId, picId)
+{
+    elem(clickScoreId).innerHTML = clickScore;
+    elem(multId).innerHTML = mult;
+    elem(counterId).innerHTML = clicks;
+}
