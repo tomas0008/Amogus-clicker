d3d3a78d26e6a5dbe87e6a7c7db92e278a0e1cad
tomas0008 <133876500+tomas0008@users.noreply.github.com>
2024-12-04 12:51:35 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -4,42 +4,55 @@ function elem(elemId)
 }
 
 
-let count = 0;
+let clickScore = 1;
+let clicks = 0;
 let mult = 1;
 let boost = 1;
+let upgrades = 0;
+let level = 1;
+
 const defMult = 1.5;
+const HIDE_ASS = {
+    1: "amogus_front1.jpeg"
+};
+const SHOW_ASS = {
+    1: "amogus_back1.gif"
+};
 
 function addOne(elemId)
 {
-    elem(elemId).innerHTML = Number((count += 1 * mult * boost).toFixed(2));
+    elem(elemId).innerHTML = Number((clicks += 1 * mult * boost).toFixed(2));
 }
 
-function upgrade(elemId, multElemId, nextUpId)
+function upgrade(elemId, multElemId, nextUpId, clickScoreId, upgradesId)
 {
-    if (count < mult*10)
+    if (clicks < mult*10)
         return 0;
     
     let el = elem(elemId);
     let multEl = elem(multElemId);
     
-    count -= mult*10;
-    count = Number(count.toFixed(2));
+    clicks -= mult*10;
+    clicks = Number(clicks.toFixed(2));
     mult *= defMult;
     mult = mult.toFixed(2);
     
     multEl.innerHTML = mult;
-    el.innerHTML = count;
+    el.innerHTML = clicks;
     elem(nextUpId).innerHTML = mult * 10;
+    
+    clickScore = Number((mult / 10).toFixed()) + 1;
+    clickScore = clickScore ? clickScore : 1;
+    elem(clickScoreId).innerHTML = clickScore;
+
+    elem(upgradesId).innerHTML = ++upgrades;
     
     return 1;
 }
 
-function maxUpgrade(elemId, multElemId, nextUpId)
+function maxUpgrade(elemId, multElemId, nextUpId, clickScoreId, upgradesId)
 {
-    while (upgrade(elemId, multElemId, nextUpId))
-    {
-        
-    }
+    while (upgrade(elemId, multElemId, nextUpId, clickScoreId, upgradesId)) {}
 }
 
 function showImage(elemId)
@@ -54,23 +67,25 @@ function hideImage(elemId)
 
 function showAss(elemId, boostId)
 {
-    elem(elemId).src = "amogus_back.gif";
+    elem(elemId).src = SHOW_ASS[level];
     boost = 2;
     elem(boostId).innerHTML = "Boost!!!";
 }
 
 function hideAss(elemId, boostId)
 {
-    elem(elemId).src = "amogus_front.jpeg";
+    elem(elemId).src = HIDE_ASS[level];
     boost = 1;
     elem(boostId).innerHTML = "";
 }
 
-function reset(elemId, multElemId, nextUpId)
+function reset(elemId, multElemId, nextUpId, clickScoreId, upgradesId)
 {
-    count = 0;
-    elem(elemId).innerHTML = count;
+    clicks = 0;
+    elem(elemId).innerHTML = clicks;
     mult = 1;
     elem(multElemId).innerHTML = mult;
     elem(nextUpId).innerHTML = Number(mult * 10).toFixed(2);
+    elem(clickScoreId).innerHTML = 1;
+    elem(upgradesId).innerHTML = 0;
 }
